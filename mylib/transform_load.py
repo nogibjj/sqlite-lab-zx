3ecb75226e24b36248d268807771e2c8037ac681
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-10-23 23:28:50 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -22,11 +22,14 @@ def load(dataset="data/Grocery.csv"):  # 修改为相对路径
     payload = csv.reader(open(dataset, newline=''), delimiter=',')
     conn = sqlite3.connect('GroceryDB.db')
     c = conn.cursor()
+    # 修改创建表的 SQL 语句
     c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id INT PRIMARY KEY,general_name,"
-              "count_products, ingred_FPro, avg_FPro_products," 
-              "avg_distance_root, ingred_normalization_term,"
-              "semantic_tree_name, semantic_tree_node)")
+    c.execute("CREATE TABLE GroceryDB (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+              "general_name TEXT, count_products INT, ingred_FPro TEXT, "
+              "avg_FPro_products REAL, avg_distance_root REAL, "
+              "ingred_normalization_term TEXT, semantic_tree_name TEXT, "
+              "semantic_tree_node TEXT)")
+
     
     c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
