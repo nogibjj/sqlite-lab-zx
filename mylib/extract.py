f7c5507c50aceb074835675a94ac389d6f9d536e
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-10-23 22:31:29 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -6,16 +6,16 @@ food dataset
 import requests
 import pandas as pd
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/Grocery.csv"):
-    """"Extract a url to a file path"""
+def extract(
+    url="https://raw.githubusercontent.com/Barabasi-
+Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
+    file_path="data/Grocery.csv"
+):
+    """Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
-    df=pd.read_csv(file_path)
-    df_sub=df.head(66)
+    df = pd.read_csv(file_path)
+    df_sub = df.head(66)
     df_sub.to_csv(file_path, index=False)
     return file_path
-
-
-
