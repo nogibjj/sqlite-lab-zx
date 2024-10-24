bd95a4162ae26c92641b1ddc42c5da293cf3b207
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-10-23 23:25:26 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -10,11 +10,15 @@ import csv
 import os
 
 #load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab-zx/data/Grocery.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
+def load(dataset="data/Grocery.csv"):  # 修改为相对路径
+    """Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
     print(os.getcwd())
+
+    if not os.path.isfile(dataset):
+        print(f"Error: The dataset '{dataset}' does not exist.")
+        return None
+
     payload = csv.reader(open(dataset, newline=''), delimiter=',')
     conn = sqlite3.connect('GroceryDB.db')
     c = conn.cursor()
@@ -23,9 +27,10 @@ def load(dataset="/workspaces/sqlite-lab-zx/data/Grocery.csv"):
               "count_products, ingred_FPro, avg_FPro_products," 
               "avg_distance_root, ingred_normalization_term,"
               "semantic_tree_name, semantic_tree_node)")
-    #insert
+    
     c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
     conn.close()
     return "GroceryDB.db"
+
 
