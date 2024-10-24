d4937c59c761192192bdd072dc8d971994f206b2
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-10-23 22:46:34 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,7 +1,9 @@
 """
 Transforms and Loads data into the local SQLite3 database
 Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
+,general name,count_products,ingred_FPro,avg_FPro_products,
+avg_distance_root,ingred_normalization_term,semantic_tree_name,
+semantic_tree_node
 """
 import sqlite3
 import csv
